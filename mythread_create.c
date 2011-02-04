e43bd79d4dcb560d6fc9bad1299f07e8cb6009da
Jitesh Shah <jitesh.1337@gmail.com>
2011-02-03 20:56:28 -0500

    Write a separate function for main_tcb for clarity reasons
    Signed-off-by: Jitesh Shah <jitesh.1337@gmail.com>

--- mythread_create.c
+++ mythread_create.c
@@ -1,5 +1,6 @@
 #include <malloc.h>
 #include <string.h>
+#include <stdlib.h>
 
 #include <signal.h>
 #include <errno.h>
@@ -23,6 +24,31 @@ mythread_t *idle_tcb;
 mythread_t *main_tcb;
 extern struct futex gfutex;
 
+static void __mythread_add_main_tcb()
+{
+	DEBUG_PRINTF("add_main_tcb: Creating node for Main thread \n");
+	main_tcb = (mythread_t *)malloc(sizeof(mythread_t));
+	if (main_tcb == NULL) {
+		DEBUG_PRINTF("add_main_tcb: Error allocating memory for main node\n");
+		exit(1);
+	}
+
+	main_tcb->start_func = NULL;
+	main_tcb->args = NULL;
+	main_tcb->state = READY;
+	main_tcb->returnValue = NULL;
+	main_tcb->blockedForJoin = NULL;
+
+	/* Get the main's tid and put it in its corresponding tcb. */
+	main_tcb->tid = __mythread_gettid();
+
+	/* Initialize futex to zero*/
+	futex_init(&main_tcb->sched_futex, 1);
+
+	/* Put it in the Q of thread blocks */
+	mythread_q_add(main_tcb);
+}
+
 int mythread_create(mythread_t * new_thread_ID,
 		    mythread_attr_t * attr,
 		    void *(*start_func) (void *), void *arg)
@@ -41,36 +67,20 @@ int mythread_create(mythread_t * new_thread_ID,
 			   | CLONE_CHILD_CLEARTID | CLONE_SYSVSEM);
 
 	if (mythread_q_head == NULL) {
+		/* This is the very first mythread_create call. Set up the Q first with tcb nodes for main thread. */
+		__mythread_add_main_tcb();
 
-	  /* This is the very first mythread_create call. Set up the Q first with tcb nodes for main thread. */
-	  DEBUG_PRINTF("create: Creating node for Main thread \n");
-	  main_tcb = (mythread_t *)malloc(sizeof(mythread_t));
-	  main_tcb->start_func = NULL;
-	  main_tcb->args = NULL;
-	  main_tcb->state = READY;
-	  main_tcb->returnValue = NULL;
-	  main_tcb->blockedForJoin = NULL;
+		/* Initialise the global futex */
+		futex_init(&gfutex, 1);
 
-	  /* Get the main's tid and put it in its corresponding tcb. */
-	  tid = __mythread_gettid();
-	  main_tcb->tid = tid;
-
-	  /* Initialize futex to zero*/
-	  futex_init(&main_tcb->sched_futex, 1);
-	  futex_init(&gfutex, 1);
-
-	  /* Put it in the Q of thread blocks */
-	  mythread_q_add(main_tcb);
-	  
-	  /* Now create the node for Idle thread. */
-	  DEBUG_PRINTF("Creating node for Idle thread \n");
-	  idle_tcb = (mythread_t *)malloc(sizeof(mythread_t));
-	  if(idle_tcb == NULL) {
-	    printf("malloc error!");
-	    return(-ENOMEM);
-	  }
-	  mythread_create(idle_tcb, NULL, mythread_idle, NULL);
-
+	  	/* Now create the node for Idle thread. */
+		DEBUG_PRINTF("create: creating node for Idle thread \n");
+		idle_tcb = (mythread_t *)malloc(sizeof(mythread_t));
+		if(idle_tcb == NULL) {
+			DEBUG_PRINTF("create: malloc error while allocating memory to idle node!");
+	  		exit(1);
+		}
+		mythread_create(idle_tcb, NULL, mythread_idle, NULL);
 	}
 
 	/*Stack-size argument is not provided */
@@ -115,33 +125,22 @@ int mythread_create(mythread_t * new_thread_ID,
 	/* Save the tid returned by clone system call in the tcb. */
 	new_thread_ID->tid = tid;
 
-	DEBUG_PRINTF("create: This should be printed first %ld\n",(unsigned long)new_thread_ID->tid);
-	//futex_up(&new_thread_ID->sched_futex);
-
+	DEBUG_PRINTF("create: Finished initialising new thread: %ld\n", (unsigned long)new_thread_ID->tid);
 	return 0;
 }
 
 int mythread_wrapper(void *thread_tcb)
 {
-
 	mythread_t *new_tcb;
-	new_tcb = (mythread_t *) thread_tcb;
+	new_tcb = (mythread_t *)thread_tcb;
 
-	DEBUG_PRINTF("Wrapper: will sleep on futex: %ld %d\n", (unsigned long)new_tcb->tid, new_tcb->sched_futex.count); fflush(stdout);
+	DEBUG_PRINTF("Wrapper: will sleep on futex: %ld %d\n", (unsigned long)__mythread_gettid(), new_tcb->sched_futex.count);
 	futex_down(&new_tcb->sched_futex);
-	DEBUG_PRINTF("Wrapper: futex value: %ld %d\n", (unsigned long)new_tcb->tid, new_tcb->sched_futex.count); fflush(stdout);
+	DEBUG_PRINTF("Wrapper: futex value: %ld %d\n", (unsigned long)new_tcb->tid, new_tcb->sched_futex.count);
 	new_tcb->start_func(new_tcb->args);
-
-	/* Do a down on the futex corresponding to this tcb. */
-	/* Call the thread_func. */
-	/* Exit the current thread. */
-
-	/* Ideally we shouldn't reach here, but if the thread doesn't call mythread_exit, we should */
-	//mythread_exit(NULL);
 
 	/* Ideally we shouldn't reach here */
 	return 0;
-
 }
 
 void * mythread_idle(void *phony)
